311be71fa242242ea68b6be3732706ed3d581c6f
Andrii Kisil <kisilam@ukr.net>
2019-10-31 02:13:02 +0200

    add php functional to send mail

--- freecontactformprocess.php
+++ freecontactformprocess.php
@@ -24,9 +24,9 @@
  *  Or upgrade to the more professional version at: https://www.freecontactform.com/responsive_form.php
  */
 
-if(isset($_POST['Email_Address'])) {
+if(isset($_POST['phone'])) {
 	
-	include 'freecontactformsettings.php';
+	include 'settings.php';
 	
 	function died($error) {
 		echo "Sorry, but there were error(s) found with the form you submitted. ";
@@ -36,42 +36,42 @@ if(isset($_POST['Email_Address'])) {
 		die();
 	}
 	
-	if(!isset($_POST['Full_Name']) ||
-		!isset($_POST['Email_Address']) ||
-		!isset($_POST['Telephone_Number']) ||
-		!isset($_POST['Your_Message']) || 
-		!isset($_POST['AntiSpam'])		
+	if(!isset($_POST['name']) ||
+//		!isset($_POST['Email_Address']) ||
+		!isset($_POST['phone']) ||
+//		!isset($_POST['Your_Message']) || 
+//		!isset($_POST['AntiSpam'])		
 		) {
 		died('Sorry, there appears to be a problem with your form submission.');		
 	}
 	
-	$full_name = $_POST['Full_Name']; // required
-	$email_from = $_POST['Email_Address']; // required
-	$telephone = $_POST['Telephone_Number']; // not required
-	$comments = $_POST['Your_Message']; // required
-	$antispam = $_POST['AntiSpam']; // required
+	$full_name = $_POST['name']; // required
+//	$email_from = $_POST['Email_Address']; // required
+	$telephone = $_POST['phone']; // not required
+//	$comments = $_POST['Your_Message']; // required
+//	$antispam = $_POST['AntiSpam']; // required
 	
 	$error_message = "";
 	
-	$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-  if(preg_match($email_exp,$email_from)==0) {
-  	$error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
+//	$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+// if(preg_match($email_exp,$email_from)==0) {
+ // 	$error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+//  }
   if(strlen($full_name) < 2) {
   	$error_message .= 'Your Name does not appear to be valid.<br />';
   }
-  if(strlen($comments) < 2) {
-  	$error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
+//  if(strlen($comments) < 2) {
+//  	$error_message .= 'The Comments you entered do not appear to be valid.<br />';
+//  }
   
-  if($antispam <> $antispam_answer) {
-	$error_message .= 'The Anti-Spam answer you entered is not correct.<br />';
-  }
+//  if($antispam <> $antispam_answer) {
+//	$error_message .= 'The Anti-Spam answer you entered is not correct.<br />';
+//  }
   
   if(strlen($error_message) > 0) {
   	died($error_message);
   }
-	$email_message = "Form details below.\r\n";
+	$email_message = "Добрый день. Меня зовут" . $full_name . ", перезвоните мне" . $telephone . " \r\n";
 	
 	function clean_string($string) {
 	  $bad = array("content-type","bcc:","to:","cc:");
@@ -87,7 +87,7 @@ $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 mail($email_to, $email_subject, $email_message, $headers);
-header("Location: $thankyou");
-echo "<script>location.replace('$thankyou')</script>";
+//header("Location: $thankyou");
+//echo "<script>location.replace('$thankyou')</script>";
 }
 die();
